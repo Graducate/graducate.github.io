e727b97d845434b72e46a2482e328d39192afeb0
Eskil Fogelström <eskil@eskilfogelstrom.com>
2015-07-15 15:47:31 +0200

    Gruntfile with CNAME

--- Gruntfile.js
+++ Gruntfile.js
@@ -278,8 +278,8 @@ module.exports = function (grunt) {
             // Explicitly add any files your site needs for distribution here.
             '_bower_components/jquery/jquery.min.js',
             'favicon.ico',
+            'CNAME',
             'apple-touch*.png',
-            'CNAME'
           ],
           dest: '<%= yeoman.dist %>'
         }]
